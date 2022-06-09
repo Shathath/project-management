ef61e7a3613046c21f8fac1292a403241e456634
Shathath <shathathrahman@gmail.com>
2022-06-09 18:29:50 +0530

    task list view from project router

---
+++ client/src/components/emptyScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+function EmptyScreen()
+{
+	return <img src="../images/emptyscreen.svg" />
+}
+
+export default EmptyScreen;
--- client/src/projectsTaskList.js
+++ client/src/projectsTaskList.js
@@ -1,16 +1,20 @@
 import React, {useEffect, useState} from 'react';
+
 import {useDispatch, useSelector} from 'react-redux';
+
 import { fetchTasksByProjectId } from './features/projectSlice';
 
 import { useParams } from 'react-router-dom';
 
+import EmptyScreen from './components/emptyScreen';
+
 function ProjectsTaskList()
 {
-	const { id } = useParams();
+	const { id } 	= useParams();
 
-	const dispatch = useDispatch();
+	const dispatch 	= useDispatch();
 
-	const tasks = useSelector((state) => state.projectsVsTasks );
+	const tasks 	= useSelector((state) => state.projectsVsTasks );
 
 	const isLoading = useSelector((state) => state.isLoading );
 
@@ -20,27 +24,49 @@ function ProjectsTaskList()
 
 	},[]);
 
+	const renderHeaderTaskList = () => 
+	{
+		var headers = [ { name :'ID', column : 'col-1' }, { name:'Title', column:'col-3'},{ name: 'Priority', column:'col-1' } , { name : 'Due Date', column:'col-1'},{ name : 'Assigned to', column:'col-2' }, { name : 'Created by', column : 'col-2'}, { name : 'Status', column : 'col-2' }];
+		
+		return  <div className="row">
+					{ headers.map((val ) => {
+						return <div className={	`${val.column} bold` }>{val.name}</div>
+					})} 	
+				</div>
+	}
+
 	const renderProjectTaskList = () => 
 	{
 		if( Object.keys(tasks).length == 0 ) 
 		{
-			return ""
+			return <EmptyScreen />;
 		}
 
 		let projectTasks = tasks[id];
 
+
 		return projectTasks.map((task) => 
 		{
 			const { task_id : id , task_name : name } = task;
-			
-			return <div key={id}>{name}</div>
+
+			return <div className="row">
+						<div className="col-1">{id}</div>
+						<div className="col-3">{name}</div>
+				   </div>
 		})
 	}
 
 	return (
-	<React.Fragment>
-		{ isLoading ? <h1>Loading</h1> : renderProjectTaskList() }	   
-	</React.Fragment>)
+		<React.Fragment>
+			{ 
+				isLoading ? <h1>Loading</h1> : 
+				<section className="container">
+					{ renderHeaderTaskList() }
+					{ renderProjectTaskList() }
+				</section>
+			}	   
+		</React.Fragment>
+	)
 }
 
 export default ProjectsTaskList;
